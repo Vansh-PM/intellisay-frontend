a35721c4fcb38b6ada0e12e2fbfce0fe855939e8
Vansh-PM <vansh.agrawal48@gmail.com>
2025-04-12 16:21:24 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: '.',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    },
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
